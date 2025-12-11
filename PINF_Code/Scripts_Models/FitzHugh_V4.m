1ca4a4b87e1fd3885363f2e66749d83c2d85b2dd
JuanDiaz <67231886+munozdjp@users.noreply.github.com>
2025-12-11 10:33:40 +0300

    Update FitzHugh_V4.m
    Code is automatized to create the trajectories dataset "inline"

--- PINF_Code/Scripts_Models/FitzHugh_V4.m
+++ PINF_Code/Scripts_Models/FitzHugh_V4.m
@@ -24,89 +24,81 @@ if ~exist(plotsDir,'dir')
     mkdir(plotsDir);
 end
 
-%% Number of variables
-% %coefficient of the right polinomial (polinomial of order 4 ...
-% %that perform the desired behavior (increasing))
-%  
-% % p1 =         1.1; 
-% % p2 =      0.9285;
-% % p3 =    -0.02771;
-% % p4 =   0.0003581;
-% % p1 =         5;
-% % p2 =      0;
-% % p3 =    0;
-% % p4 =   0;
-% p1 =         1.1; 
-% p2 =      0.09285;
-% p3 =    0;
-% p4 =   0;
+% Number of variables
+%coefficient of the right polinomial (polinomial of order 4 ...
+%that perform the desired behavior (increasing))
+
+p1 =         1.1; 
+p2 =      0.09285;
+p3 =    0;
+p4 =   0;
+
+epsilon=14;
+a= 0.06;
+lambda=0.5;
+n = 4; % Number of variables
+dt=0.0001; % Timestep
+
+% Integrate Interval of time. 
+%(How to create interval of time given ...
+%...interval of beta parameteer)
+Initinterval = 250;
+neg_inter = 0;
+tinterval=[neg_inter Initinterval];
+tspan=[.001:dt:tinterval(2)-tinterval(1)];
+N = length(tspan);
+options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,n));
+X=[];
+
+%Coeficients of polyorder 3; p1+p2*t+p3*t^2+p4*t^3 & and initial time value 
+c=[p1,p2,p3,p4,Initinterval];
+yzero=c(1)+c(2)*(tspan)+c(3)*(tspan).^2+c(4)*(tspan).^3;
+maxBeta = max(yzero);
+yzero = yzero/maxBeta;
+
+%Plot of inverted polynomial
+figure(1)
+plot(tspan,yzero,'r-')
+xlabel('time')
+ylabel('beta parameter')
+hold on
+xline(0,'k--');
+set(gca,'FontSize',16);
+l=legend('Explicit polinomial for steady state generation');
+l.FontSize = 14;
+l.Location='northeast';
+title('Inverted polinomial');
+hold off
+xMax = ones(4)
+traslation=0; %traslation of the system in y coordinate
+%% integration of the lorentz system
+%     i=i+1
+
+reescaletime = 1;
+
+for mu0=[yzero(1)]; %initial condition of Beta
+    for u0 = 0.048498469; %Iniitial contions for x and y [x0,x0,z0]        
+        w0 = 0.80830782; %initial condition for z0
+        %[t,x] = ode45(@(t,x) Fitz_Nagumo2th(t,x,c,a,lambda,epsilon),tspan,[tspan(1),mu0,u0,w0],options);
+        [t,x] = ode23s(@(t,x) Fitz_Nagumo2th(t,x,c,a,lambda,epsilon,xMax,reescaletime,maxBeta),tspan,[tspan(1),mu0,u0,w0],options);
+         X = [X;x];
+            % Plot of state variable vs Beta parameter
+            figure(2)
+            hold on
+            plot(x(:,2),x(:,3),'b-')
+            xlabel('Beta')
+            ylabel('x_3')
+            xline(0,'k--');
+            set(gca,'FontSize',16);
+            l=legend('Trajectory using numerical integration');
+            l.FontSize = 14;
+            l.Location='northeast';
+            title('State variable vs Biffurcation parameter')
+            hold off    
+    end
+end
+save('variablesFitzHug')
 % 
-% epsilon=14;
-% a= 0.06;
-% lambda=0.5;
-% n = 4; % Number of variables
-% dt=0.0001; % Timestep
-% 
-% % Integrate Interval of time. 
-% %(How to create interval of time given ...
-% %...interval of beta parameteer)
-% Initinterval = 250;
-% neg_inter = 0;
-% tinterval=[neg_inter Initinterval];
-% tspan=[.001:dt:tinterval(2)-tinterval(1)];
-% N = length(tspan);
-% options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,n));
-% X=[];
-% 
-% %Coeficients of polyorder 3; p1+p2*t+p3*t^2+p4*t^3 & and initial time value 
-% c=[p1,p2,p3,p4,Initinterval];
-% yzero=c(1)+c(2)*(tspan)+c(3)*(tspan).^2+c(4)*(tspan).^3;
-% maxBeta = max(yzero);
-% yzero = yzero/maxBeta;
-% 
-% %Plot of inverted polynomial
-% figure(1)
-% plot(tspan,yzero,'r-')
-% xlabel('time')
-% ylabel('beta parameter')
-% hold on
-% xline(0,'k--');
-% set(gca,'FontSize',16);
-% l=legend('Explicit polinomial for steady state generation');
-% l.FontSize = 14;
-% l.Location='northeast';
-% title('Inverted polinomial');
-% hold off
-% xMax = ones(4)
-% traslation=0; %traslation of the system in y coordinate
-% %% integration of the lorentz system
-% %     i=i+1
-% 
-% reescaletime = 1;
-% 
-% for mu0=[yzero(1)]; %initial condition of Beta
-%     for u0 = 0.048498469; %Iniitial contions for x and y [x0,x0,z0]        
-%         w0 = 0.80830782; %initial condition for z0
-%         %[t,x] = ode45(@(t,x) Fitz_Nagumo2th(t,x,c,a,lambda,epsilon),tspan,[tspan(1),mu0,u0,w0],options);
-%         [t,x] = ode23s(@(t,x) Fitz_Nagumo2th(t,x,c,a,lambda,epsilon,xMax,reescaletime,maxBeta),tspan,[tspan(1),mu0,u0,w0],options);
-%          X = [X;x];
-%             % Plot of state variable vs Beta parameter
-%             figure(2)
-%             hold on
-%             plot(x(:,2),x(:,3),'b-')
-%             xlabel('Beta')
-%             ylabel('x_3')
-%             xline(0,'k--');
-%             set(gca,'FontSize',16);
-%             l=legend('Trajectory using numerical integration');
-%             l.FontSize = 14;
-%             l.Location='northeast';
-%             title('State variable vs Biffurcation parameter')
-%             hold off    
-%     end
-% end
-% save('variablesFitzHug')
-%% 
 load('variablesFitzHug.mat')
 %%
 %Mu observed from equations of steady state
